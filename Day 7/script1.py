195dcda7ad59a103956d2da37da4adeef2eb7099
vchapandrews <95453696+vchapandrews@users.noreply.github.com>
2021-12-22 18:15:10 +0000

    Add files via upload

---
+++ Day 7/script1.py
@@ -0,0 +1,15 @@
+with open("input.txt") as text_file:
+    input_data = [int(i) for i in text_file.read().split(",")]
+
+targets_to_try = [i for i in range(min(input_data), max(input_data) + 1)]
+position_options = {}
+
+for target in targets_to_try:
+    input_data_copy = input_data.copy()
+    for idx in range(len(input_data_copy)):
+        difference = abs(input_data_copy[idx] - target)
+        position_options[target] = position_options.get(target, 0) + difference
+
+best_option = min(position_options, key=position_options.get)
+
+print(f"The most fuel efficient horizontal position is {best_option} which uses {position_options[best_option]} units of fuel.")
